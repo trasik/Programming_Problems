b98ea9af17e74c6a0cf4c8c379f7a33ceaea7e11
Torendra Rasik <torendrarasik@gmail.com>
2023-01-07 15:57:23 -0500

    feat(problems): Finished the second and third problem sets

--- src/Jan12023/Driver.java
+++ src/Jan12023/Driver.java
@@ -3,16 +3,37 @@ package Jan12023;
 public class Driver {
 
     public static void main(String[] args) {
+        //Problem 1
         int[] firstSet = {1, 1, 0, 1, 1, 1};
         int[] secondSet = {1, 0, 1, 1, 0, 1};
-        int[] thirdSet = {};
-        int[] fourthSet = null;
+        int[] emptySet = {};
+        int[] nullSet = null;
         System.out.println(findMaxConsecutiveOnes(firstSet));
         System.out.println(findMaxConsecutiveOnes(secondSet));
-        System.out.println(findMaxConsecutiveOnes(thirdSet));
-        System.out.println(findMaxConsecutiveOnes(fourthSet));
+        System.out.println(findMaxConsecutiveOnes(emptySet));
+        System.out.println(findMaxConsecutiveOnes(nullSet));
+
+        //Problem 2
+
+        int[] firstSet2 = {12,345,2,6,7896};
+        int[] secondSet2 = {555,901,482,1771};
+        System.out.println(findNumbers(firstSet2));
+        System.out.println(findNumbers(secondSet2));
+        System.out.println(findNumbers(emptySet));
+        System.out.println(findNumbers(nullSet));
+
+        //Problem 3
+
+        int[] firstSet3 = {-4,-1,0,3,10};
+        int[] secondSet3 = {-7,-3,2,3,11};
+
+        printArray(sortedSquares(firstSet3));
+        printArray(sortedSquares(secondSet3));
+        printArray(sortedSquares(emptySet));
+        printArray(sortedSquares(nullSet));
     }
 
+    //Problem One
     private static int findMaxConsecutiveOnes(int[] nums) {
         if(nums == null || nums.length == 0) return -1;
 
@@ -29,5 +50,56 @@ public class Driver {
         }
 
         return maxOnes;
+    }
+
+    //Problem 2
+    private static int findNumbers(int[] nums) {
+        if(nums == null || nums.length == 0) return -1;
+        int total = 0;
+
+        for (int num : nums) {
+            if (totalDigits(num) % 2 == 0) total++;
+        }
+
+        return total;
+    }
+
+    private static int totalDigits(int num) {
+        int digits = 0;
+
+        while(num > 0) {
+            digits++;
+            num /= 10;
+        }
+
+        return digits;
+    }
+
+    //Problem 3
+    private static int[] sortedSquares(int[] nums) {
+        if(nums == null || nums.length == 0) return new int[0];
+        int start = 0;
+        int end = nums.length - 1;
+        int k = nums.length - 1;
+        int[] result = new int[nums.length];
+
+        while(start <= end) {
+            if(Math.abs(nums[start]) > Math.abs(nums[end])) {
+                result[k--] = nums[start] * nums[start];
+                start++;
+            } else {
+                result[k--] = nums[end] * nums[end];
+                end--;
+            }
+        }
+
+        return result;
+    }
+
+    private static void printArray(int[] arr) {
+        for(int a : arr) {
+            System.out.print(a + ",");
+        }
+        System.out.println();
     }
 }
--- src/Jan12023/Problems.md
+++ src/Jan12023/Problems.md
@@ -13,3 +13,39 @@
 `Input: nums = [1,1,0,1,1,1]
 Output: 3
 Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.`
+
+##2. Find Numbers with Even Number of Digits
+
+---
+**Prompt:** Given an array nums of integers, return how many of them contain an even number of digits.
+
+**Constraints:**
+- 1 <= nums.length <= 500
+- 1 <= nums[i] <= 10<sup>5</sup>
+
+**Example:**
+`Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation:
+12 contains 2 digits (even number of digits).
+345 contains 3 digits (odd number of digits).
+2 contains 1 digit (odd number of digits).
+6 contains 1 digit (odd number of digits).
+7896 contains 4 digits (even number of digits).
+Therefore only 12 and 7896 contain an even number of digits.`
+
+##3. Squares of a Sorted Array
+
+---
+**Prompt:** Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+**Constraints:**
+- 1 <= nums.length <= 10<sup>4</sup>
+- -10<sup>4</sup> <= nums[i] <= 10<sup>4</sup>
+- nums is sorted in non-decreasing order.
+
+**Example:**
+`Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].`
