3e673004910ac7a655af5d5a085806cb30898150
Torendra Rasik <torendrarasik@gmail.com>
2023-01-07 14:03:49 -0500

    feat(problem): First problem solved.

--- src/Jan12023/Driver.java
+++ src/Jan12023/Driver.java
@@ -3,6 +3,31 @@ package Jan12023;
 public class Driver {
 
     public static void main(String[] args) {
-        System.out.println("Working");
+        int[] firstSet = {1, 1, 0, 1, 1, 1};
+        int[] secondSet = {1, 0, 1, 1, 0, 1};
+        int[] thirdSet = {};
+        int[] fourthSet = null;
+        System.out.println(findMaxConsecutiveOnes(firstSet));
+        System.out.println(findMaxConsecutiveOnes(secondSet));
+        System.out.println(findMaxConsecutiveOnes(thirdSet));
+        System.out.println(findMaxConsecutiveOnes(fourthSet));
+    }
+
+    private static int findMaxConsecutiveOnes(int[] nums) {
+        if(nums == null || nums.length == 0) return -1;
+
+        int maxOnes = 0;
+        int consecutiveOnes = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                consecutiveOnes++;
+                maxOnes = Math.max(maxOnes, consecutiveOnes);
+            } else {
+                consecutiveOnes = 0;
+            }
+        }
+
+        return maxOnes;
     }
 }
---
+++ src/Jan12023/Problems.md
@@ -0,0 +1,15 @@
+#January 7, 2023 Problems
+
+##1. Max Consecutive Ones
+
+---
+**Prompt:** Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+**Constraints:** 
+- 1 <= nums.length <= 10<sup>5</sup>
+- nums[i] is either 0 or 1.
+
+**Example:**
+`Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.`
