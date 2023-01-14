d9400198e21838485a459e6decb674443d3f378e
Torendra Rasik <torendrarasik@gmail.com>
2023-01-14 13:55:19 -0500

    feat(problems): Best Time To Buy and Sell Stock - Solves the buy sell stock problem through a linear scan through the days

--- src/Jan142023/Driver.java
+++ src/Jan142023/Driver.java
@@ -3,6 +3,7 @@ package Jan142023;
 public class Driver {
 
     public static void main(String[] args) {
+        //Problem 1
         ListNode list1 = new ListNode(3);
         ListNode cycle = new ListNode(2);
         list1.next = cycle;
@@ -11,8 +12,21 @@ public class Driver {
         list1.next.next.next.next = cycle;
 
         System.out.println(detectCycle(list1).val);
+
+        //Problem 2
+
+        int[] firstSet = {7,1,5,3,6,4};
+        int[] secondSet = {7,6,4,3,1};
+        int[] emptySet = {};
+        int[] nullSet = null;
+
+        System.out.println(maxProfit(firstSet));
+        System.out.println(maxProfit(secondSet));
+        System.out.println(maxProfit(emptySet));
+        System.out.println(maxProfit(nullSet));
     }
 
+    //Problem 1
     public static ListNode detectCycle(ListNode head) {
         if(head == null) return null;
 
@@ -37,6 +51,20 @@ public class Driver {
         }
 
         return null;
+    }
+
+    public static int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return -1;
+
+        int buy = prices[0];
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < buy) buy = prices[i];
+            profit = Math.max(profit, prices[i] - buy);
+        }
+
+        return profit;
     }
 
     public static void printList(ListNode node) {
