0d5aadc9a7a7e67b65351dcea897f58bfa66e9c2
Torendra Rasik <torendrarasik@gmail.com>
2023-01-17 13:24:49 -0500

    feat(problems): Lowest Common Ancestor of a Binary Search Tree - Solves the LCA of BST problem through recursion as it checks each node to see if each parent node fits the conditions

--- src/Jan172023/Driver.java
+++ src/Jan172023/Driver.java
@@ -9,6 +9,14 @@ public class Driver {
         node.right = new TreeNode(3);
 
         System.out.println("Is Valid?: " + isValidBST(node));
+
+        //Problem 2
+        TreeNode temp1 = new TreeNode(4, new TreeNode(3), new TreeNode(5));
+        TreeNode temp2 = new TreeNode(2, new TreeNode(0), temp1);
+        TreeNode temp3 = new TreeNode(8, new TreeNode(7), new TreeNode(9));
+        TreeNode root = new TreeNode(6, temp2, temp3);
+
+        System.out.println("LCA: " + lowestCommonAncestor(root, temp2, temp3).val);
     }
 
     //Problem 1
@@ -23,5 +31,14 @@ public class Driver {
         if(node.val <= min || node.val >= max) return false;
 
         return isValidBSTHelper(node.left, min, node.val) && isValidBSTHelper(node.right, node.val, max);
+    }
+
+    //Problem 2
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return root;
+
+        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        else if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        else return root;
     }
 }
--- src/Jan172023/Problems.md
+++ src/Jan172023/Problems.md
@@ -20,3 +20,22 @@ Both the left and right subtrees must also be binary search trees.
 **Example:**
 `Input: root = [2,1,3]
 Output: true`
+
+## 2. Lowest Common Ancestor of a Binary Search Tree
+
+---
+**Prompt:** Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+**Constraints:**
+- The number of nodes in the tree is in the range [2, 10<sup>5</sup>]. 
+- -10<sup>9</sup> <= Node.val <= 10<sup>9</sup> 
+- All Node.val are unique. 
+- p != q 
+- p and q will exist in the BST.
+
+**Example:**
+`Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.`
