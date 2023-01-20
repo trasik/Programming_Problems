d9c5bb68db90a0840b8e3a7276471fee0cfc5b35
Torendra Rasik <torendrarasik@gmail.com>
2023-01-20 13:47:59 -0500

    feat(problems): Unique Paths - Solves the unique paths problem by pre populating first row and column with 1's and using those together to calculate next path item

--- src/Jan202023/Driver.java
+++ src/Jan202023/Driver.java
@@ -10,8 +10,13 @@ public class Driver {
         System.out.println(minCostClimbingStairs(cost1));
         System.out.println(minCostClimbingStairs(cost2));
 
+        //Problem 2: Unique Paths
+        System.out.println(uniquePaths(3, 7));
+        System.out.println(uniquePaths(3, 2));
+
     }
 
+    //Problem 1
     private static int minCostClimbingStairs(int[] cost) {
         if(cost == null || cost.length == 0) return 0;
 
@@ -28,5 +33,29 @@ public class Driver {
         if(cache[n] != 0) return cache[n];
         cache[n] = cost[n] + Math.min(minCostCSHelper(cost, cache, n - 1), minCostCSHelper(cost, cache, n - 2));
         return cache[n];
+    }
+
+    //Problem 2: Unique Paths
+
+    public static int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+
+        //Populate the first row and column with 1's
+        for(int i = 0; i < m; i++) {
+            grid[i][0] = 1;
+        }
+
+        for(int i = 0; i < n; i++) {
+            grid[0][i] = 1;
+        }
+
+        //Calculate the iTH and jTH grid item based on previous row and previous column
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
+            }
+        }
+
+        return grid[m - 1][n - 1];
     }
 }
--- src/Jan202023/Problems.md
+++ src/Jan202023/Problems.md
@@ -19,3 +19,19 @@ Output: 15
 Explanation: You will start at index 1.
 Pay 15 and climb two steps to reach the top.
   The total cost is 15.`
+
+## 2. Unique Paths
+
+---
+**Prompt:** There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+**Constraints:**
+- 1 <= m, n <= 100
+
+**Example:**
+`Input: m = 3, n = 7
+Output: 28`
