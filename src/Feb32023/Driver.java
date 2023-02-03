3c50555cec766806c1a8027e1b1c988e20ce52e3
Torendra Rasik <torendrarasik@gmail.com>
2023-02-03 13:39:30 -0500

    feat(problems): Climbing Stairs
    - Solves the climbing stairs problem through memoization through recursion. Basically it's the addition of the previous steps taken either by 1 step or 2 steps aka the Fibonacci Sequence.

--- src/Feb32023/Driver.java
+++ src/Feb32023/Driver.java
@@ -12,6 +12,11 @@ public class Driver {
         System.out.println("Insert Index: " + searchInsert(arr, target1));
         System.out.println("Insert Index: " + searchInsert(arr, target2));
         System.out.println("Insert Index: " + searchInsert(arr, target3));
+
+        //Problem 2: Climbing Stairs
+        System.out.println("Number of Ways to Climb " + 2 + " Steps: " + climbStairs(2));
+        System.out.println("Number of Ways to Climb " + 3 + " Steps: " + climbStairs(3));
+        System.out.println("Number of Ways to Climb " + 40 + " Steps: " + climbStairs(40));
     }
 
     //Problem 1: Search Insert Position
@@ -30,5 +35,17 @@ public class Driver {
         }
 
         return start;
+    }
+
+    //Problem 2: Climbing Stairs
+    static int[] cache = new int[46];
+
+    public static int climbStairs(int n) {
+        if(cache[n] != 0) return cache[n];
+
+        if(n < 3) return n;
+
+        cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return cache[n];
     }
 }
--- src/Feb32023/Problems.md
+++ src/Feb32023/Problems.md
@@ -16,3 +16,20 @@ You must write an algorithm with O(log n) runtime complexity.
 **Example:**
 `Input: nums = [1,3,5,6], target = 5
 Output: 2`
+
+## 2. Climbing Stairs
+
+---
+**Prompt:** You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+**Constraints:**
+- 1 <= n <= 45
+
+**Example:**
+`Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps`
