25440fb015a91a75c04771b97af3ce5d1ec2e39c
Torendra Rasik <torendrarasik@gmail.com>
2023-02-04 16:29:39 -0500

    feat(problems): Symmetric Tree
    - Solves symmetric tree problem with the use of recursion check each left value to the right side on a mirror scale

--- src/Feb42023/Driver.java
+++ src/Feb42023/Driver.java
@@ -13,6 +13,13 @@ public class Driver {
 
         System.out.println(Arrays.toString(inorderTraversal(tree1).toArray()));
 
+        //Problem 2: Symmetric Tree
+        TreeNode tree2 = new TreeNode(1);
+        tree2.setLeft(new TreeNode(2, new TreeNode(3), new TreeNode(4)));
+        tree2.setRight(new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+
+        System.out.println("Is Given Tree Symmetric?: " + isSymmetric(tree2));
+
     }
 
     //Problem 1: Binary Tree Inorder Traversal
@@ -28,5 +35,20 @@ public class Driver {
         inorderHelper(node.getLeft(), list);
         list.add(node.getVal());
         inorderHelper(node.getRight(), list);
+    }
+
+    //Problem 2: Symmetric Tree
+    public static boolean isSymmetric(TreeNode root) {
+        if(root == null) return false;
+
+        return isSymmetricHelper(root.getLeft(), root.getRight());
+    }
+
+    private static boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if(left == null || right == null) return left == right;
+
+        if(left.getVal() != right.getVal()) return false;
+
+        return isSymmetricHelper(left.getLeft(), right.getRight()) && isSymmetricHelper(left.getRight(), right.getLeft());
     }
 }
