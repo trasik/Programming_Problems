d2efc66fb2c5bb2fe07d9e35e23f8616c254ef38
Torendra Rasik <torendrarasik@gmail.com>
2023-02-05 13:30:23 -0500

    feat(problems): Best Time to Buy and Sell Stock - Solves this problem through a linear pass updating the buy everytime it's smaller than the current

--- src/Feb52023/Driver.java
+++ src/Feb52023/Driver.java
@@ -11,6 +11,13 @@ public class Driver {
         System.out.println(Arrays.toString(generate(5).toArray()));
         System.out.println(Arrays.toString(generate(1).toArray()));
         System.out.println(Arrays.toString(generate(10).toArray()));
+
+        //Problem 2: Best Time to Buy and Sell Stock
+        int[] prices1 = {7,1,5,3,6,4};
+        int[] prices2 = {7,6,4,3,1};
+
+        System.out.println("Max Profit for Prices " + Arrays.toString(prices1) + ": " + maxProfit(prices1));
+        System.out.println("Max Profit for Prices " + Arrays.toString(prices2) + ": " + maxProfit(prices2));
     }
 
     //Problem 1: Pascal's Triangle
@@ -37,5 +44,19 @@ public class Driver {
         }
 
         return triangle;
+    }
+
+    //Problem 2: Best Time to Buy and Sell Stock
+    public static int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+        int profit = 0;
+        int buy = prices[0];
+
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] < buy) buy = prices[i];
+            profit = Math.max(profit, prices[i] - buy);
+        }
+
+        return profit;
     }
 }
--- src/Feb52023/Problems.md
+++ src/Feb52023/Problems.md
@@ -13,3 +13,22 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 **Example:**
 `Input: numRows = 5
 Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]`
+
+## 2. Best Time to Buy and Sell Stock
+
+---
+**Prompt:** You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+**Constraints:**
+- 1 <= prices.length <= 10<sup>5</sup> 
+- 0 <= prices[i] <= 10<sup>4</sup>
+
+**Example:**
+`Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.`
